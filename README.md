8cbfb2cc18ad53ff0ca4d7be09924434dccb771d
Rajith Delantha <rajith.delantha@aurea.com>
2017-10-26 11:31:03 +0530

    update files and remove file

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-readme
+readme - append line
--- test/New.java
+++ test/New.java
@@ -2,6 +2,6 @@ public class New
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World - AWS SQS!!" );
+        System.out.println( "Hello World!! - AWS SQS!!" );
     }
 }
